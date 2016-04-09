4b0878a1e66ca921a2cf71a43a7f516bcad4cfdb
monstergelo <bimawansatrianto@gmail.com>
2016-04-09 13:20:04 +0700

    tambah opsi, 1 deterministic, 2 non-de

--- MainActivity.java
+++ MainActivity.java
@@ -1,27 +1,40 @@
+import java.util.*;
+
 class MainActivity
 {
     public static void main(String[] args)
     {
-        String aturan = "@A B\n1 A B # b.\n1 A C # c.\n1 B B b b.\n1 C C c c.";
+        String aturan = "@A B\n1 A C # b.\n1 A B # b.\n1 B B b b.\n1 C C c c.\n1 C C b c.";
         String input = "1111";
 
         MesinPDA mesin = new MesinPDA();
-/*
-        if(mesin.cekGrammar(aturan))
-        {
-            System.out.println(mesin.parseDeterministic(input));
-            System.out.println(mesin.getCatatan());
-        }else{
-            System.out.println(mesin.errorGrammar());
-        }
-*/
-        if(mesin.cekGrammar(aturan))
-        {
-            System.out.println(mesin.parseNonDeterministic(input));
-            System.out.println(mesin.getCatatan());
-        }else{
-            System.out.println(mesin.errorGrammar());
-        }
+        Scanner in = new Scanner(System.in); 
+        int opsi = in.nextInt();
 
+        switch(opsi)
+        {
+            case 1:
+            {
+                if(mesin.cekGrammar(aturan))
+                {
+                    System.out.println(mesin.parseDeterministic(input));
+                    System.out.println(mesin.getCatatan());
+                }else{
+                    System.out.println(mesin.errorGrammar());
+                }
+                break;
+            }
+            case 2:
+            {
+                if(mesin.cekGrammar(aturan))
+                {
+                    System.out.println(mesin.parseNonDeterministic(input));
+                    System.out.println(mesin.getCatatan());
+                }else{
+                    System.out.println(mesin.errorGrammar());
+                }
+                break;
+            }
+        }
     }
 }
